67dd6c15a6d86c8ed3aa958e8468f2f292787705
Nwatu endurance <endurancenwatu9@gmail.com>
2022-10-13 15:18:59 -0700

    m

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,9 +3,9 @@
  *  main - Print a text
  *
  *  Return: Always 1
- */ 
+ */
 int main(void)
 {
-	write(2,"and that piece of art is usedful\". Dors Korpar, 2015-10-19\n",59);
+	write(2, "and that piece of art is usedful\". Dors Korpar, 2015-10-19\n", 59);
 	return (1);
 }
