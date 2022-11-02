4d23d634234713482619edbf91c38645cc281418
Nwatu endurance <endurancenwatu9@gmail.com>
2022-11-02 13:15:01 -0700

    fixed

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,9 +1,9 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
 int _putchar(char);
 void _puts_recursion(char *s);
-void_print_rev_recursion(char *s);
+void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
