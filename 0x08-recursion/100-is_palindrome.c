685f023f2f4afb5f09bdf3c678036da9f821f763
Nwatu endurance <endurancenwatu9@gmail.com>
2022-11-02 16:28:15 -0700

    error

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * _strlen_recursion - returns the length of a string.
  * @s: string
@@ -20,7 +19,6 @@ int _strlen_recursion(char *s)
  * @n2: biggest iterator.
  * Return: .
  */
-
 int comparator(char *s, int n1, int n2)
 {
 	if (*(s + n1) == *(s + n2))
@@ -30,4 +28,16 @@ int comparator(char *s, int n1, int n2)
 	return (0 + comparator(s, n1 + 1, n2 - 1));
 	}
 	return (0);
+}
+
+/**
+ *  is_palindrome - detects if a string is a palindrome.
+ *  @s: string.
+ *  Return: 1 if s is a palindrome, 0 if not.
+ */
+int is_palindrome(char *s)
+{
+	if (*s == '\0')
+	return (1);
+	return (comparator(s, 0, _strlen_recursion(s) - 1));
 }
