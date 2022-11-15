fd4d23a7b8d2fc20548bb1dd0c8e076eaa742f60
Nwatu endurance <endurancenwatu9@gmail.com>
2022-11-14 17:42:41 -0800

    pi.h_main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
 
 /**
- * main - prints the name of the file.
+ *  main - prints the name of the file program was compiled from
  *
- * Return: Always 0.
+ *  Return: Always 0
  */
+
 int main(void)
+
+
 {
-		printf("%s\n", FILE);
-		return (0);
+	printf("%s\n", __FILE__);
+
+	return (0);
+
+
 }
